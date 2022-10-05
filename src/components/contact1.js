4ee03bfc470b2e40adaca17f8f6250549e3c230d
Chawapon <chawapon2001@gmail.com>
2022-10-05 23:53:02 +0700

    moon addres + balance

---
+++ src/components/contact1.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+const { ethers } = require("ethers");
+
+function Contract1() {
+  return (
+    <div>
+      <h4> {"Contract"} </h4>
+      <button>hello</button>
+    </div>
+  );
+}
+
+export default Contract1;
