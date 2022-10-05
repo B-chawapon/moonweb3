1601d1620960e7a3f6e606bd279666d441535192
Chawapon <chawapon2001@gmail.com>
2022-10-06 01:06:20 +0700

    intrega smart contract

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import simple_token_abi from "./abi/contractNFT.json";
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 const { ethers } = require("ethers");
+const BN = require("bn.js");
 // const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
-//const provider = new ethers.providers.Web3Provider(window.ethereum);
+const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 function App() {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -12,7 +14,45 @@ function App() {
   const [userBalance, setUserBalance] = useState(null);
   const [connButtonText, setConnButtonText] = useState("Connect Wallet");
 
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const [nftName, setnftName] = useState("FUCK");
+  const [supply, setSupply] = useState(null);
+  const [transferHash, setTransferHash] = useState(null);
+
   let contractAddress = "0xad5A1F2B9b0B9A76BbD708036B3833558f3136B3";
+  const contracttest = new ethers.Contract(
+    contractAddress,
+    simple_token_abi,
+    provider
+  );
+
+  const lol = async () => {
+    console.log(1);
+    let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log(2);
+    let tempSigner = tempProvider.getSigner();
+    console.log(3);
+    let tempContract = new ethers.Contract(
+      contractAddress,
+      simple_token_abi,
+      tempSigner
+    );
+    console.log(4);
+    let a = await tempContract.name();
+    let b = await tempContract.symbol();
+    let c = await tempContract.totalSupply();
+    let d = await tempContract.owner();
+    let z = await tempContract.tokenURI(0);
+    //c = ethers.utils.formatEther(c);
+    console.log(defaultAccount);
+    let y = await tempContract.balanceOf(defaultAccount);
+
+    console.log(a, b, c, c.toNumber(), c.toHexString(), d, z, y.toNumber());
+  };
+  lol();
 
   const connectWalletHandler = () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
